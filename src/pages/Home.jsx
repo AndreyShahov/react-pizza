299d9778124f423230ae782000437216ca0e56d6
Andrey Shahov <andreyka.shakhov@mail.ru>
2023-07-16 11:15:12 +0300

    refactor: add sort in redux

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { setCategoryId } from '../reudx/slices/filterSlice';
 
 import Categories from "../components/Categories";
 import Sort from "../components/Sort";
@@ -8,21 +11,24 @@ import Pagination from '../components/Pagination';
 import { SearchContext } from '../App';
 
 const Home = () => {
-  const { searchValue } = React.useContext(SearchContext)
+  const dispatch = useDispatch();
+  const { categoryId, sort } = useSelector(state => state.filter);
+  const sortType = sort.sortProperty;
+
+  const { searchValue } = React.useContext(SearchContext);
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
-  const [categoryId, setCategoryId] = React.useState(0);
   const [currentPage, setCurrentPage] = React.useState(1);
-  const [sortType, setSortType] = React.useState({
-    name: 'популярности',
-    sortProperty: 'rating'
-  });
+
+  const onChangeCategory = (id) => {
+    dispatch(setCategoryId(id));
+  }
 
   React.useEffect(() => {
     setIsLoading(true);
 
-    const sortBy = sortType.sortProperty.replace('-', '');
-    const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc';
+    const sortBy = sortType.replace('-', '');
+    const order = sortType.includes('-') ? 'asc' : 'desc';
     const category = categoryId > 0 ? `category=${categoryId}` : '';
     const search = searchValue ? `&search=${searchValue}` : '';
 
@@ -44,8 +50,8 @@ const Home = () => {
     <>
       <div className="container">
         <div className="content__top">
-          <Categories value={categoryId} onChangeCategory={(i) => setCategoryId(i)} />
-          <Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
+          <Categories value={categoryId} onChangeCategory={onChangeCategory} />
+          <Sort />
         </div>
         <h2 className="content__title">Все пиццы</h2>
         <div className="content__items">
